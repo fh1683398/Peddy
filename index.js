ad31ad8bf7ab6d0d78888f42fd130185904de89b
fh1683398 <fh1683398@gmail.com>
2026-03-01 19:03:14 +0600

    adopt button functionality done

--- index.js
+++ index.js
@@ -39,17 +39,17 @@ const showAllPets = (pets) => {
             </div>
             <div class="mt-6">
                 <h4 class="text-xl text-[#131313] font-bold mb-2">${pet.pet_name}</h4>
-                <p class="text-gray-600"><i class="fa-solid fa-cubes-stacked text-sm"></i>  Breed: ${pet.breed ? pet.breed : "Not available"}</p>
-                <p class="text-gray-600"><i class="fa-regular fa-calendar text-sm"></i>  Birth: ${pet.date_of_birth}</p>
-                <p class="text-gray-600"><i class="fa-solid fa-mercury text-sm"></i>  Gender: ${pet.gender}</p>
-                <p class="text-gray-600"><i class="fa-solid fa-dollar-sign text-sm"></i>  Price: ${pet.price}$</p>
+                <p class="text-gray-600"><i class="fa-solid fa-cubes-stacked text-sm"></i>  Breed:  ${pet.breed ? pet.breed : "Not available"}</p>
+                <p class="text-gray-600"><i class="fa-regular fa-calendar text-sm"></i>  Birth:  ${pet.date_of_birth}</p>
+                <p class="text-gray-600"><i class="fa-solid fa-mercury text-sm"></i>  Gender:  ${pet.gender}</p>
+                <p class="text-gray-600"><i class="fa-solid fa-dollar-sign text-sm"></i>  Price: $${pet.price}</p>
             </div>
             <hr class="my-4 border-[#e5e7eb]">
             <div class="flex gap-4">
 
                 <button class="flex-1 border border-gray-300 p-2 rounded-lg text-[#0e7a81] hover:bg-[#0e7a81] hover:text-white"><i class="fa-regular fa-heart"></i></button>
 
-                <button class="flex-1 border border-gray-300 p-2 rounded-lg font-bold text-[#0e7a81] hover:bg-[#0e7a81] hover:text-white">Adopt</button>
+                <button onclick="adoptPet(${pet.petId}, this)" class="flex-1 border border-gray-300 p-2 rounded-lg font-bold text-[#0e7a81] hover:bg-[#0e7a81] hover:text-white">Adopt</button>
 
                 <button onclick="petDetails(${pet.petId})" id="${pet.petId}" class="flex-1 border border-gray-300 p-2 rounded-lg font-bold text-[#0e7a81] hover:bg-[#0e7a81] hover:text-white">Details</button>
             </div>
@@ -69,7 +69,7 @@ const showPetDetails = (data) => {
     const detailsModal = document.getElementById('detailsModal')
     detailsModal.classList.remove('none')
 
-    const { image, breed, date_of_birth, price, gender, vaccinated_status, pet_name, pet_details, petId } = data;
+    const { image, breed, date_of_birth, price, gender, vaccinated_status, pet_name, pet_details } = data;
     const card = document.createElement('div')
     card.className = 'p-5 md:p-7 rounded-xl bg-white max-w-[600px]'
     card.innerHTML = `
@@ -79,25 +79,48 @@ const showPetDetails = (data) => {
             <div class="text-gray-600">
                 <div class="flex gap-4 items-center mb-2"><i class="fa-solid fa-cubes-stacked"></i> Breed: ${breed ? breed : "Not available"}</div>
                 <div class="flex gap-4 items-center mb-2"><i class="fa-solid fa-mercury"></i> Gender: ${gender ? gender : "Not available"}</div>
-                <div class="flex gap-4 items-center mb-2"><i class="fa-solid fa-syringe md:mb-0"></i> Vaccinated status: ${vaccinated_status? vaccinated_status : "Not available"}</div>
+                <div class="flex gap-4 items-center mb-2"><i class="fa-solid fa-syringe md:mb-0"></i> Vaccinated status: ${vaccinated_status ? vaccinated_status : "Not available"}</div>
             </div>
             <div class="text-gray-600">
                 <div class="flex gap-4 items-center mb-2"><i class="fa-regular fa-calendar"></i> Birth: ${date_of_birth ? date_of_birth : "Not available"}</div>
-                <div class="flex gap-4 items-center mb-2"><i class="fa-solid fa-dollar-sign md:mb-0"></i>  Price: $${price? price : "Not available"}</div>
+                <div class="flex gap-4 items-center mb-2"><i class="fa-solid fa-dollar-sign md:mb-0"></i>  Price: $${price ? price : "Not available"}</div>
             </div>
         </div>
         <hr class="my-4 border-[#e5e7eb]">
         <div>
             <h4 class="font-semibold mb-3 text-gray-700">Details Information</h4>
             <p class="mb-4 text-gray-600">${pet_details}</p>
-            <button onclick="closeModal()" class="w-full bg-gray-200 py-2 rounded-lg">Cancel</button>
+            <button onclick="closeModal()" class="w-full bg-[rgba(14,122,129,0.1)] text-lg text-[#0e7a81] border border-[#0e7a81] py-2 rounded-lg hover:bg-[#0e7a81] hover:text-white">Cancel</button>
         </div>
     `
     detailsModal.innerHTML = ""
     detailsModal.append(card)
-    console.log(card)
 }
+//Load Adopt button
+const adoptPet = (id, button) => {
+    const adoptModal = document.getElementById('adoptModal')
+    let countdownEl = document.getElementById('countdown')
 
+    let count = 3
+    adoptModal.classList.remove('none')
+
+    countdownEl.innerText = count;
+    const interval = setInterval(() => {
+        count--
+        countdownEl.innerText = count;
+
+        if (count === 0) {
+            clearInterval(interval)
+            adoptModal.classList.add('none')
+            count.innerText = 3
+        }
+    }, 1000)
+
+    button.innerText = 'Adopted'
+    button.classList.add('opacity-50')
+    button.style.cursor = "not-allowed"
+    button.disabled = true;
+}
 
 
 
@@ -108,7 +131,11 @@ const detailsModal = document.getElementById('detailsModal')
 const closeModal = () => {
     detailsModal.classList.add('none')
 }
-document.getElementById('closeBtn')
+detailsModal.addEventListener('click', (e) => {
+    if (e.target === detailsModal) {
+        closeModal()
+    }
+})
 
 loadCategories()
 loadPets()
